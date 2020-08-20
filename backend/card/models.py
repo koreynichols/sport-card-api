2e0b42519a671deb6aed3520bdbb14a5ff14e76a
Korey Nichols <koreynichols1@gmail.com>
2020-08-19 21:06:14 -0700

    working on creating card model

--- backend/card/models.py
+++ backend/card/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class card(models.Model):
+    player_name = models.charField(maxLength = 50)
+    card_company = models.charField(maxLength = 20)
+    card_set = models.charField(maxLenght = 20)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    sport = models.charField(maxLength = 15)
+    team = models.charField(maxLength = 50)
+    rookie_card = models.BooleanField()
+    auto = models.BooleanField()
+    relic = models.BooleanField()
+    relic_type = models.charField(maxLength = 25)
+    numbered = models.BooleanField()
+    numbered_to = models.PositiveSmallIntegerField()
+    
