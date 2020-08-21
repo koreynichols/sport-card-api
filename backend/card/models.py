46e16b5d4e7807186243dd9cc97d979246eb34b5
Korey Nichols <koreynichols1@gmail.com>
2020-08-20 19:42:58 -0700

    fixed card model

--- backend/card/models.py
+++ backend/card/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class card(models.Model):
-    player_name = models.charField(maxLength = 50)
-    card_company = models.charField(maxLength = 20)
-    card_set = models.charField(maxLenght = 20)
+    player_name = models.CharField(max_length = 50)
+    card_company = models.CharField(max_length = 20)
+    card_set = models.CharField(max_length = 20)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
-    sport = models.charField(maxLength = 15)
-    team = models.charField(maxLength = 50)
+    sport = models.CharField(max_length = 15)
+    team = models.CharField(max_length = 50)
     rookie_card = models.BooleanField()
     auto = models.BooleanField()
     relic = models.BooleanField()
-    relic_type = models.charField(maxLength = 25)
+    relic_type = models.CharField(max_length = 25)
     numbered = models.BooleanField()
     numbered_to = models.PositiveSmallIntegerField()
     card_img = models.ImageField(upload_to='images/')
