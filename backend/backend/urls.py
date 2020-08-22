7509ea34a1ea88c94299ddc1114e70adb1760174
Korey Nichols <koreynichols1@gmail.com>
2020-08-21 20:16:32 -0700

    added rest framework

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from card import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.CardViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- backend/card/views.py
+++ backend/card/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Card
+from rest_framework import viewsets
+from .serializers import CardSerializer
 # Create your views here.
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
